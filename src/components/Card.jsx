7b52f5dea60ca2783fa6581600f1f3c068d4016a
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-12-12 19:03:00 +0100

    add css alla card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,18 +8,12 @@ export default function Card({ title, original_title, original_language, image, 
 
 
   return (
-    <div className={style.container}>
-      <div className={style.row}>
-        <div className={style.col}>
-          <div className={style.card}>
-            {/* <img src={`https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png`} alt="" /> */}
-            <p>{title}</p>
-            <p>{original_language}</p>
-            <p>{original_title}</p>
-            <p>{vote}</p>
-          </div>
-        </div>
-      </div>
+    <div className={style.card}>
+      {/* <img src={`https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png`} alt="" /> */}
+      <p>{title}</p>
+      <p>{original_language}</p>
+      <p>{original_title}</p>
+      <p>{vote}</p>
     </div>
   )
 }
